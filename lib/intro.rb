e3e081a517700ca3f5dc7df2a7671a91b14a570c
Carina Sweet <carina.sweet06@gmail.com>
2021-05-19 07:31:32 -0600

    Update intro class

--- lib/intro.rb
+++ lib/intro.rb
@@ -5,9 +5,11 @@ class Introduction
 
   decision = gets.chomp
 
-  intro.play if decision == 'p'
-  intro.instructions if decision == 'i'
-  intro.quit if decision == 'q'
+  # intro.play if decision == 'p'
+  # intro.instructions if decision == 'i'
+  # intro.quit if decision == 'q'
+
+  if decision = 'p'
 
   attr_reader :play
 
@@ -15,17 +17,29 @@ class Introduction
       @decision = []
     end
 
-    def play ## Game Flow Class
-      p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
-      p "What's your guess?"
-      guess = gets.chomp()
+    def player_input(decision)
+      if decision = "i"
+        p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
+      elsif decision = "p"
+        p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+        p "What's your guess?"
+           # guess = gets.chomp()
+       elsif decision = "q"
+         puts 'byeeeeeeee'
+      end
     end
 
-    def instructions #print instructions
-      p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
-    end
-
-    def quit(q) #stop game
-      puts 'byeeeeeeee'
-    end
-end
+#     def play ## Game Flow Class
+#       p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+#       p "What's your guess?"
+#       # guess = gets.chomp()
+#     end
+#
+#     def instructions #print instructions
+#       p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
+#     end
+#
+#     def quit(q) #stop game
+#       puts 'byeeeeeeee'
+#     end
+# end
