2f13209405b5608c6be816dab097f75ec9ae76ac
Christina Delpone <cdelpone@gmail.com>
2021-05-19 08:30:37 -0600

    Update intro class and test file

--- spec/intro_spec.rb
+++ spec/intro_spec.rb
@@ -3,9 +3,18 @@ require './lib/intro'
 RSpec.describe Intro do
 
   it "exists" do
-    decision = Intro.new('p')
+    decision = Intro.new("p")
 
     expect(decision).to be_an_instance_of(Intro)
   end
+
+  #  it "reads instructions" do
+  #   decision = Intro.new("p")
+  #   decision_i = Intro.new("i")
+  #   decision_q = Intro.new("q")
+  #
+  #   expect(decision).to be_an_instance_of(Intro)
+  #   expect(intro.player_input).to eq(decision)
+  # end
 
 end
