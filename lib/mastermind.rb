c4bf3aee9cc12859326db294382346d98ccc7cbb
Christina Delpone <cdelpone@gmail.com>
2021-05-18 20:47:27 -0600

    Change mastermind

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,6 +1,28 @@
 require './lib/game_flow'
 require './lib/code_generator'
 
+class Intro
+  attr_reader :play
+
+    def initialize
+      @decision = []
+    end
+
+    def play
+      p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+      p "What's your guess?"
+      guess = gets.chomp()
+    end
+
+    def instructions
+      p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
+    end
+
+    def quit(q)
+      puts 'byeeeeeeee'
+    end
+end
+
 
   p "Welcome to MASTERMIND!"
   p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
@@ -15,21 +37,6 @@ require './lib/code_generator'
   class GameFlow
   attr_reader :decision
 
-  def initialize
-    @decision = []
-  end
-
-  def play
-    p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
-    p "What's your guess?"
-    guess = gets.chomp()
-
-
-  end
-
-  def instructions
-    p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
-  end
 
   def quit
   end
