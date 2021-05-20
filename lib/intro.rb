e8258b49f5c5d81bfd2ccb26155a409e57a932d5
Carina Sweet <carina.sweet06@gmail.com>
2021-05-20 07:30:02 -0600

    Refactor intro start game method

--- lib/intro.rb
+++ lib/intro.rb
@@ -1,27 +1,23 @@
+class Introduction
+  # attr_reader :decision
 
-
-class Intro
-
-  attr_reader :decision
-
-    def initialize(decision)
-      @decision = gets.chomp
+    def initialize
+      # @decision = gets.chomp
     end
 
     def start_game
       p "Welcome to MASTERMIND!"
       p "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-      # input = gets.chomp
+      input = gets.chomp.downcase
 
-      if @decision == "i"
+      if input == "i" || "instructions"
         p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
-        #### add while input = i ask p or q
-      # elsif input == "p"
-      #   p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
-      #   p "What's your guess?"
-           # guess = gets.chomp()
-       elsif
-       #input == "q"
+        ### add while input = i ask p or q
+      elsif input == "p" || "play"
+        p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+        # push into the gameflow
+
+      elsif input == "q" || "quit"
          p "byeeeeeeee"
          exit
       end
