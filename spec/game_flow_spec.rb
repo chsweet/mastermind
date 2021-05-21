2ea9c1908e9acc1ec7584699bee617034d965291
Carina Sweet <carina.sweet06@gmail.com>
2021-05-20 19:48:00 -0600

    Add new print instructions test

--- spec/game_flow_spec.rb
+++ spec/game_flow_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe GameFlow do
     expect(game).to be_an_instance_of(GameFlow)
   end
 
+  it 'prints instructions' do
+    game = GameFlow.new
+
+    expect(game.start_game).to eq("The objective of the game is to guess the exact positions of the colors in the computer's sequence")
+  end
 end
