c02eeba1644f34b0842398930412f09188f3fea2
Christina Delpone <cdelpone@gmail.com>
2021-05-24 16:46:48 -0600

    Refactor game flow class and spec file

--- spec/game_flow_spec.rb
+++ spec/game_flow_spec.rb
@@ -1,4 +1,3 @@
-require './lib/intro'
 require './lib/game_flow'
 
 RSpec.describe GameFlow do
@@ -11,7 +10,15 @@ RSpec.describe GameFlow do
 
   it 'prints instructions' do
     game = GameFlow.new
-
+    allow(game).to receive(:gets).and_return("I\n")
     expect(game.start_game).to eq("The objective of the game is to guess the exact positions of the colors in the computer's sequence")
+  end
+
+  it 'plays game' do
+    allow(CodeGenerator).to receive(:secret_code).and_return("rrgb")
+
+    game = GameFlow.new
+    allow(game).to receive(:gets).and_return("RRGB\n")
+    expect(game.play_game).to eq('Congrats!')
   end
 end
