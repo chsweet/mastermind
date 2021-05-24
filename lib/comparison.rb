eca2651364e34561f8d1ad4d7b59f06fcad7cde3
Christina Delpone <cdelpone@gmail.com>
2021-05-24 16:27:26 -0600

    Refactor comparison class and spec file

--- lib/comparison.rb
+++ lib/comparison.rb
@@ -1,35 +1,34 @@
-# require './lib/code_generator'
-#
-# class Comparison
-#   attr_reader :player_guess, :secret_code
-#
-#   def initialize
-#     @code = CodeGenerator.new
-#     @player_guess = @code.player_guess
-#     @secret_code = @code.secret_code
-#     @player_guess_split = @player_guess.chars
-#     @secret_code_split = @secret_code.chars
-#   end
-#
-#   def elements_correct
-#     intersection = @player_guess_split & @secret_code_split
-#
-#     if !intersection.empty?
-#       intersection.size
-#     else
-#     end
-#
-#   end
-#
-#   def elements_position
-#     pairs = @player_guess_split.zip(@secret_code_split)
-#
-#     pairs.count { |guess,code| guess==code }
-#   end
-#
-#   def feedback
-#       "#{@player_guess.upcase} has #{elements_correct} of the correct elements with #{elements_position} in the correct positions."
-#       # You've taken #{guess_count} guess"
-#   end
-#
-# end
+class Comparison
+attr_accessor :guess_count, :player_guess
+
+  def initialize(secret_code, player_guess=nil, guess_count=nil)
+    @player_guess = player_guess
+    @guess_count = guess_count
+    @code_split = secret_code.chars
+  end
+
+  def elements_correct
+    intersection = player_guess_split & @code_split
+
+    if !intersection.empty?
+      intersection.size
+    else
+    end
+  end
+
+  def elements_position
+    pairs = player_guess_split.zip(@code_split)
+
+    pairs.count { |guess,code| guess==code }
+  end
+
+  def feedback
+      "#{player_guess.upcase} has #{elements_correct} of the correct elements with #{elements_position} in the correct positions. You've taken #{@guess_count} guess"
+  end
+
+  private
+  #can't be called outside this class; test everything above
+  def player_guess_split
+    @player_guess.chars
+  end
+end
