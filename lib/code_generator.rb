d372643c27deb74d7a6140f42406ea6a962bb6d8
Christina Delpone <cdelpone@gmail.com>
2021-05-24 16:05:48 -0600

    Refactor code generator class and spec file

--- lib/code_generator.rb
+++ lib/code_generator.rb
@@ -1,38 +1,13 @@
 class CodeGenerator
 
-  def initialize
-    # @secret_code = []
-    # @player_input = []
-  end
+  # def initialize
+  #   # @secret_code = []
+  # end
 
   #Will need to change if beginner, intermidiate, advance
-  def secret_code
+  def self.secret_code
     colors = ['r', 'g', 'b', 'y']
     @secret_code = Array.new(4) {colors.sample}.join
   end
-
-  # def player_guess
-  #   @player_guess = gets.chomp.downcase
-  #
-  #   #I think it may be better to call on the methods in the game flow
-  #   # player_force_quit
-  #   # player_guess_length
-  # end
-
-  # def player_guess_length
-  #
-  #   if @player_guess.length > 4
-  #     p "you have too many"
-  #   elsif @player_guess.length < 4
-  #     p "you are missing a letter"
-  #   end
-  # end
-  #
-  # def player_force_quit
-  #   if @player_guess == 'c' || @player_guess == 'cheat'
-  #     p @secret_code
-  #   elsif @player_guess == 'q' || @player_guess == 'quit'
-  #   end
-  # end
 
 end
