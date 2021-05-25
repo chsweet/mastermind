76f44cff4314cd0dbd0d5c788c9726ff5f54428b
Carina Sweet <carina.sweet06@gmail.com>
2021-05-25 15:45:51 -0600

    Remove comments

--- lib/comparison.rb
+++ lib/comparison.rb
@@ -10,8 +10,8 @@ attr_accessor :guess_count, :player_guess
   def elements_correct
     intersection = player_guess_split & @code_split
     intersection.empty? ? 0: intersection.size
-    # intersection == ['a','b','c'] ? 0: intersection.size
   end
+
 
   def elements_position
     pairs = player_guess_split.zip(@code_split)
