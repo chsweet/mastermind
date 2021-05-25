2aa83505d5490cff45c356a0de0056472acc5142
Carina Sweet <carina.sweet06@gmail.com>
2021-05-24 19:21:43 -0600

    Update player_guess_count error

--- lib/game_flow.rb
+++ lib/game_flow.rb
@@ -1,11 +1,9 @@
-require './lib/code_generator'
-
 class GameFlow
 
   def initialize
     @code = CodeGenerator.secret_code
     @player_guess = nil
-    @game_guess_count = 0
+    @guess_count = 0
     @comparison = Comparison.new(@code, nil, nil)
   end
 
@@ -16,7 +14,14 @@ class GameFlow
 
     if input == "i" || input == "instructions"
       p "The objective of the game is to guess the exact positions of the colors in the computer's sequence"
-      ## add while input = i ask p or q
+      ### more instruction
+      p "Would you like to (p)lay or (quit)?"
+      input = gets.chomp.downcase
+      if input == "p" || input == "play"
+        play_game
+      elsif input == "q" || input == "quit"
+        quit
+      end
     elsif input == "p" || input == "play"
       play_game
     elsif input == "q" || input == "quit"
@@ -25,44 +30,69 @@ class GameFlow
   end
 
   def play_game
-    p "I have generated a beginner sequence with four elements made up of: (r)ed,(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+    p "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game or (c)heat to see the answer."
     p "What's your guess?"
-    @player_guess = gets.chomp.downcase
-
-    valid?
-    @game_guess_count += 1
-    @comparison.player_guess = @player_guess
-    @comparison.game_guess_count = @game_guess_count
+    player_input
+    # @guess_count += 1
+    # @player_guess = gets.chomp.downcase
+    # valid?
+    # @comparison.player_guess = @player_guess
+    # @comparison.guess_count = @guess_count
 
     while @player_guess != @code
       p @comparison.feedback
-      @game_guess_count += 1
-      @player_guess = gets.chomp.downcase
-
-      valid?
-      @comparison.player_guess = @player_guess
-      @comparison.game_guess_count = @game_guess_count
+      player_input
+      # @guess_count += 1
+      # @player_guess = gets.chomp.downcase
+      # valid?
+      # @comparison.player_guess = @player_guess
+      # @comparison.guess_count = @guess_count
     end
-    p 'Congrats!'
+      winner
   end
 
   def valid?
     if @player_guess == 'c' || @player_guess == 'cheat'
       p @code
+      p "Better luck next time!"
+      exit
     elsif @player_guess == 'q' || @player_guess == 'quit'
       quit
     elsif @player_guess.length > 4
-      p "you have too many, guess again"
+      p "You have too many letters, guess again!"
       @player_guess = gets.chomp.downcase
     elsif @player_guess.length < 4
-      p "you are missing a letter"
+      p "You are missing a letter, guess again!"
       @player_guess = gets.chomp.downcase
     end
+  end
+
+  def player_input
+    @guess_count += 1
+    @player_guess = gets.chomp.downcase
+    valid?
+    @comparison.player_guess = @player_guess
+    @comparison.guess_count = @guess_count
   end
 
   def quit
     p "Thank you for playing!"
     exit
+  end
+
+  # def game_timer
+  # end
+
+  def winner
+    p "Congratulations! You guessed the sequence #{@code} in NEED TIMER"
+    p "Do you want to (p)lay again or (quit)?"
+    input = gets.chomp.downcase
+
+    if input == "p" || input == "play"
+      play_game
+    elsif input == "q" || input == "quit"
+      quit
+    end
   end
 
 end
